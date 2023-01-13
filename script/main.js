ed264474f351818da8d115346295004064a94965
Keon <h21816595@gmail.com>
2023-01-13 10:37:31 +0800

    node-module目錄的js改換成cdnjs

---
+++ script/main.js
@@ -0,0 +1,9 @@
+
+$(function() {
+  // let button = $("#more-button").find(".btn");
+    // button.on('click',function(){
+    //   gsap.to(button, 0.2, {scale: 1.2, ease: Power3.easeIn});
+    //   gsap.to(button, 0.2, {scale: 1, delay: 0.2, ease: Power3.easeOut});
+    // });
+    console.log(1);
+});
